c105692804114759d3f754b9bf34047db3f8884e
dadondev <dadondev03@gmail.com>
2023-12-19 22:00:10 +0500

    yangilandi

---
+++ app.js
@@ -0,0 +1,90 @@
+let getUzWord = document.querySelector("#uzWords");
+let getEnWord = document.querySelector("#enWords");
+let sendBtn = document.querySelector("#sendBtn");
+let form = document.querySelector("form");
+let list = document.querySelector("#list");
+let checkList = document.querySelector("#checkList");
+let checkBtn = document.querySelector("#check");
+let delBtn = document.querySelector("#delete");
+let words = [];
+let words1 = words;
+let checkWords = [];
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  pushWord();
+});
+
+function renderWord() {
+  let html = "";
+  if (words.length) {
+    words.forEach((word) => {
+      html += `<div class="flex items-center w-full text-lg font-medium border-b-2 divide-slate-200 py-2">
+        <p class="font-medium text-lg mr-[153px]">${word.id * 1 + 1}</p>
+        <p id="enWord" class="mr-[149px]">${word.en}</p>
+        <p id="uzWord" class="mr-32">${word.uz}</p>
+        <div class="flex gap-3">
+          <button id="check">
+            <i class="fa-solid fa-circle-check" style="color: #06cf02" onclick="check(${word})"></i>
+          </button>
+          <button id="delete">
+            <i class="fa-solid fa-trash" style="color: #ff0001" onclick="del(${
+              word.id
+            })"></i>
+          </button>
+        </div>
+      </div>`;
+    });
+  }
+  list.innerHTML = html;
+}
+
+function renderCheck() {
+  let html = "";
+  if (checkWords.length) {
+    checkWords.forEach((checkWord) => {
+      html += `<div class="flex items-center w-full text-lg font-medium border-b-2 divide-slate-200 pb-2">
+            <p class="id font-medium text-lg mr-[154px]">${
+              checkWords.length * 1 + 1
+            }</p>
+            <p id="enWord" class="mr-[153px]">${checkWord.en}</p>
+            <p id="uzWord" class="mr-32">${checkWord.uz}</p>
+            <div class="flex gap-3">
+              <button id="check">
+                <i
+                  class="fa-solid fa-share fa-flip-horizontal"
+                  style="color: #3a7cee"
+                ></i>
+              </button>
+              <button id="delete">
+                <i class="fa-solid fa-trash" style="color: #ff0001 onclick="del(${
+                  checkWord.id
+                })"></i>
+              </button>
+            </div>
+          </div>`;
+    });
+  }
+  checkList.innerHTML = html;
+}
+
+function pushWord() {
+  words.push({
+    id: words.length,
+    en: getEnWord.value,
+    uz: getUzWord.value,
+  });
+  renderWord();
+}
+function del(idWord) {
+  words = words.filter((word) => word.id != idWord);
+  renderWord();
+  renderCheck();
+}
+
+function check(checkword) {
+  //   words.filter((word) => word.id == checkword.id)
+  words = words.filter((word) => word.id != checkword.id);
+  checkWords.push(words.filter((word) => word.id == checkword.id));
+  console.log(words);
+}
