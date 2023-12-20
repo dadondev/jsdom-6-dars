937c9d79f387a1d7cfb44fe129d96aba70ef7622
dadondev <dadondev03@gmail.com>
2023-12-20 11:04:37 +0500

    yangilandi

--- app.js
+++ app.js
@@ -20,12 +20,12 @@ function renderWord() {
   if (words.length) {
     words.forEach((word) => {
       html += `<div class="flex items-center w-full text-lg font-medium border-b-2 divide-slate-200 py-2">
-        <p class="font-medium text-lg mr-[153px]">${word.id * 1 + 1}</p>
+        <p class="font-medium text-lg mr-[153px]">${words.length * 1}</p>
         <p id="enWord" class="mr-[149px]">${word.en}</p>
         <p id="uzWord" class="mr-32">${word.uz}</p>
         <div class="flex gap-3">
-          <button id="check">
-            <i class="fa-solid fa-circle-check" style="color: #06cf02" onclick="check(${word})"></i>
+          <button id="check" onclick="check(${word.id})">
+            <i class="fa-solid fa-circle-check" style="color: #06cf02"></i>
           </button>
           <button id="delete">
             <i class="fa-solid fa-trash" style="color: #ff0001" onclick="del(${
@@ -45,21 +45,21 @@ function renderCheck() {
     checkWords.forEach((checkWord) => {
       html += `<div class="flex items-center w-full text-lg font-medium border-b-2 divide-slate-200 pb-2">
             <p class="id font-medium text-lg mr-[154px]">${
-              checkWords.length * 1 + 1
+              checkWords.length * 1
             }</p>
             <p id="enWord" class="mr-[153px]">${checkWord.en}</p>
             <p id="uzWord" class="mr-32">${checkWord.uz}</p>
             <div class="flex gap-3">
-              <button id="check">
+              <button id="check" onclick="returnedWord(${checkWord.id})">
                 <i
                   class="fa-solid fa-share fa-flip-horizontal"
                   style="color: #3a7cee"
                 ></i>
               </button>
               <button id="delete">
-                <i class="fa-solid fa-trash" style="color: #ff0001 onclick="del(${
-                  checkWord.id
-                })"></i>
+              <i class="fa-solid fa-trash" style="color: #ff0001" onclick="del(${
+                checkWord.id
+              })"></i>
               </button>
             </div>
           </div>`;
@@ -78,13 +78,24 @@ function pushWord() {
 }
 function del(idWord) {
   words = words.filter((word) => word.id != idWord);
+  checkWords = checkWords.filter((word) => word.id != idWord);
   renderWord();
   renderCheck();
 }
-
 function check(checkword) {
-  //   words.filter((word) => word.id == checkword.id)
-  words = words.filter((word) => word.id != checkword.id);
-  checkWords.push(words.filter((word) => word.id == checkword.id));
-  console.log(words);
+  words = words.filter((word) => word.id != checkword);
+  let newWord = words1.filter((word) => word.id == checkword);
+  checkWords.push(newWord[0]);
+  renderCheck();
+  renderWord();
+  newWord = null;
+}
+
+function returnedWord(returnWord) {
+  let newWord = checkWords.filter((word) => word.id == returnWord);
+  checkWords = checkWords.filter((word) => word.id != returnWord);
+  words.push(newWord[0]);
+  renderCheck();
+  renderWord();
+  newWord = null;
 }
